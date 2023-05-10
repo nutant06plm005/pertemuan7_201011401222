7ead46f24f3b2dee9b66c58fdfc600c6d071db1b
Nutan <nutan13ari@gmail.com>
2023-05-10 21:02:51 +0700

    salah commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,312 +10,154 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Nutan Ari Setyo Utami'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      'Shoes',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                color: Colors.white,
-                              ),
-                              child: const Icon(
-                                Icons.person,
-                                color: Color.fromARGB(31, 24, 57, 245),
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 240, 202, 240),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Nike SB Zoom Blazer',
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Text(
-                            'Mid Premium',
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Text(
-                              'S8.795',
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.network(
-                          'https://i.postimg.cc/7PBCV2B4/Nike-SB-Zoom-Blazer-Low-Pro-GT-Skate-Shoes-White-removebg-preview.png'),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 73, 242, 248),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Nike Air Zoom Pegasus',
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Text(
-                            'Mens Rood Running Shoes',
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Text(
-                              'S9.995',
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.network(
-                          'https://i.postimg.cc/GpD1qjKD/pegasus-removebg-preview.png'),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 247, 160, 208),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 6),
-                            child: Text(
-                              'Nike Zoomx Vaporfly',
-                              style: TextStyle(
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Mens Rood Racing Shoes',
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Text(
-                              'S19.695',
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.network(
-                          'https://i.postimg.cc/g0KPm35B/Nike-Zoom-X-Vaporfly-Next-removebg-preview.png'),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 6),
-                            child: Text(
-                              'Nike Air Force l S50',
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Order Kids Show',
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Text(
-                              'l Colour',
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'S6.295',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.network(
-                          'https://i.postimg.cc/pd7VgfFb/Coming-soon-Nike-Air-Force-1-Pixel-removebg-preview.png'),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 247, 247, 179),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 6),
-                            child: Text(
-                              'Nike Waffie One',
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Mens Shoes',
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Text(
-                              'S8.295',
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.network(
-                          'https://i.postimg.cc/fyznVk4T/Nike-Waffle-One-Infinite-Lilac-Marathon-Running-Shoes-Sneakers-DC2533-101-Size-US-5-removebg-prev.png'),
-                    ],
-                  ),
-                ),
-              ],
+      appBar: AppBar(
+        title: Text("Nutan Ari Setyo Utami"),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        padding: EdgeInsets.all(10.0),
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        children: <Widget>[
+          Container(
+            color: Colors.lightBlueAccent,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('One'),
+              ),
             ),
-          ],
-        ));
+          ),
+          Container(
+            color: Colors.grey,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Two'),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.grey,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Three'),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.lightGreenAccent,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Four'),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.greenAccent,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Five'),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Six'),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.teal,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Seven'),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.brown,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Eight'),
+              ),
+            ),
+          ),
+          Container(
+            color: Color.fromARGB(255, 45, 61, 47),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Nine'),
+              ),
+            ),
+          ),
+          Container(
+            color: Color.fromARGB(255, 196, 204, 127),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Ten'),
+              ),
+            ),
+          ),
+          Container(
+            color: Color.fromARGB(255, 169, 182, 97),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Eleven'),
+              ),
+            ),
+          ),
+          Container(
+            color: Color.fromARGB(255, 138, 141, 91),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Twelve'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
